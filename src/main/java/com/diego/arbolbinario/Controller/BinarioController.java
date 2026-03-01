e1968f9459d2fc1f36e1705b292f209673761a5c
Diego0528 <dandrinojimenez@ejemplo.com>
2026-03-01 16:33:12 -0600

    feat: Rediseño de app y reacion de menu principal

---
+++ src/main/java/com/diego/arbolbinario/Controller/BinarioController.java
@@ -0,0 +1,68 @@
+package com.diego.arbolbinario.Controller;
+
+import com.diego.arbolbinario.Model.ArbolBinario;
+import javafx.fxml.FXML;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+
+public class BinarioController {
+
+    public TextField txtResultado;
+    @FXML
+    private Pane overlay;
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private TextField txtNodo;
+    @FXML
+    private Button btnPreOrden;
+    @FXML
+    private Button btnInOrden;
+    @FXML
+    private Button btnPostOrden;
+
+
+    ArbolBinario arbol = new ArbolBinario();
+
+    public void initialize() {
+
+        GaussianBlur blur = new GaussianBlur();
+        blur.setRadius(10);
+
+        overlay.setEffect(blur);
+    }
+
+
+    public void ingNodo (MouseEvent mouseEvent) {
+        int x = Integer.parseInt(txtNodo.getText());
+        txtNodo.clear();
+        System.out.println(x);
+        arbol.Insertar(x);
+    }
+
+
+
+    public void preOrden(MouseEvent mouseEvent) {
+        System.out.println("PreOrden");
+        String resultado = arbol.preOrden(arbol.raiz);
+        txtResultado.setText(resultado);
+    }
+
+    public void inOrden(MouseEvent mouseEvent) {
+        System.out.println("InOrden");
+        String resultado = arbol.inOrden(arbol.raiz);
+        txtResultado.setText(resultado);
+    }
+
+    public void postOrden(MouseEvent mouseEvent) {
+        System.out.println("PostOrden");
+        String resultado = arbol.postOrden(arbol.raiz);
+        txtResultado.setText(resultado);
+    }
+
+
+}
--- src/main/java/com/diego/arbolbinario/Controller/MainController.java
+++ src/main/java/com/diego/arbolbinario/Controller/MainController.java
@@ -1,68 +1,28 @@
 package com.diego.arbolbinario.Controller;
-
-import com.diego.arbolbinario.Model.ArbolBinario;
-import javafx.fxml.FXML;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
 
-public class MainController {
+    public Pane Main;
 
-    public TextField txtResultado;
-    @FXML
-    private Pane overlay;
-    @FXML
-    private Button btnIngresar;
-    @FXML
-    private TextField txtNodo;
-    @FXML
-    private Button btnPreOrden;
-    @FXML
-    private Button btnInOrden;
-    @FXML
-    private Button btnPostOrden;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
 
 
-    ArbolBinario arbol = new ArbolBinario();
-
-    public void initialize() {
-
-        GaussianBlur blur = new GaussianBlur();
-        blur.setRadius(0);
-
-        overlay.setEffect(blur);
     }
 
-
-    public void ingNodo (MouseEvent mouseEvent) {
-        int x = Integer.parseInt(txtNodo.getText());
-        txtNodo.clear();
-        System.out.println(x);
-        arbol.Insertar(x);
+    public void goBinary() {
+        System.out.println("Ir a Binary Tree");
+        // cargar vista binary
     }
 
-
-
-    public void preOrden(MouseEvent mouseEvent) {
-        System.out.println("PreOrden");
-        String resultado = arbol.preOrden(arbol.raiz);
-        txtResultado.setText(resultado);
+    public void goAVL() {
+        System.out.println("Ir a AVL Tree");
+        // cargar vista avl
     }
-
-    public void inOrden(MouseEvent mouseEvent) {
-        System.out.println("InOrden");
-        String resultado = arbol.inOrden(arbol.raiz);
-        txtResultado.setText(resultado);
-    }
-
-    public void postOrden(MouseEvent mouseEvent) {
-        System.out.println("PostOrden");
-        String resultado = arbol.postOrden(arbol.raiz);
-        txtResultado.setText(resultado);
-    }
-
-
 }
